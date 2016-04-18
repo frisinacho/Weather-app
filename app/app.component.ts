62a7739e227e510a23df4121f01d40509a22e40c
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-04-18 13:33:37 +0200

    Adding city from constructor

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,8 +8,13 @@ import {Component} from 'angular2/core';
     selector : 'my-app',
     template : `
         <h1>Weather App</h1>
+        <h2>This is the weather forecast in {{ city }}</h2>
     `
 })
 export class AppComponent {
-    
+    city:string;
+
+    constructor() {
+        this.city = "Stockholm";
+    }
 }
