e156b50cb0fb8f024935140d107e93fa6a9b4142
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-04-18 20:51:58 +0200

    Adding getWeather function

--- app/app.component.ts
+++ app/app.component.ts
@@ -34,4 +34,24 @@ export class AppComponent {
         this.city = "";
         this.weatherOfCities = [];
     }
+
+    getWeather = function (city:string) {
+        var weather:Weather;
+        if (city.toLocaleLowerCase() == "stockholm") {
+            weather = {
+                "id" : 1,
+                "city" : "Stockholm",
+                "main" : "Clouds",
+                "description" : "Overcast clouds"
+            };
+        } else if (city.toLocaleLowerCase() == "london") {
+            weather = {
+                "id" : 2,
+                "city" : "London",
+                "main" : "Rain",
+                "description" : "Very heavy rain"
+            };
+        }
+        return weather;
+    }
 }
