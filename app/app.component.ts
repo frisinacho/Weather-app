52703c29537c1b68941e711270b5df52a32d676f
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-04-18 13:46:24 +0200

    Implementing Weather class

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,17 +4,27 @@
 
 import {Component} from 'angular2/core';
 
+import {Weather} from './weather';
+
 @Component({
     selector : 'my-app',
     template : `
         <h1>Weather App</h1>
         <h2>This is the weather forecast in {{ city }}</h2>
+        
+        <ul>
+            <li>Weather type: {{ weather.main }}</li>
+            <li>Weather description: {{ weather.description }}</li>
+        </ul>
     `
 })
 export class AppComponent {
     city:string;
+    weather:Weather;
 
     constructor() {
         this.city = "Stockholm";
+
+        this.weather = new Weather(1, "London", "cloudy", "Fully cloudy");
     }
 }
